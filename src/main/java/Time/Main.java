dc3b83049454ecdfc383d85e3b744754f2e47622
muhammed sule dabban <muhammedsulai4@gmail.com>
2025-10-28 10:22:19 +0100

    fisrt commit

---
+++ src/main/java/Time/Main.java
@@ -0,0 +1,10 @@
+package Time;
+
+public class Main {
+    public static void main(String[] args) {
+        Time time = new Time();
+
+        ();
+        System.out.println();
+    }
+}
---
+++ src/test/java/Time/TimeTest.java
@@ -0,0 +1,14 @@
+package Time;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeTest {
+    Time time = new Time();
+
+    @Test
+    void  testSetTime(){
+    }
+
+}
