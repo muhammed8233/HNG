2666a4f442e0fbeda9fdd86a40f0a6ce88395e45
muhammed sule dabban <muhammedsulai4@gmail.com>
2025-10-28 15:28:41 +0100

    update

--- src/main/java/Time/Main.java
+++ src/main/java/Time/Main.java
@@ -4,7 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Time time = new Time();
 
-        ;
+        displayTime("after object is created", time);
         System.out.println();
+
+        time.setTime(14,25,54);
+        displayTime("after calling set time", time);
+        System.out.println();
+
+        try{
+            time.setTime(89,75,65);
+        }
+        catch (IllegalArgumentException e){
+            System.out.printf("Exception: %s%n%n", e.getMessage());
+        }
+
+        displayTime("after calling set time with an invalid value", time);
+    }
+    private static void displayTime(String header, Time t){
+        System.out.printf("%s%nUniversal time: %s%nStandard time: %s%n",
+                 header, t.toUniversalString(),t.toString() );
     }
 }
