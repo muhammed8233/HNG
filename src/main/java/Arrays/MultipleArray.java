69fc35c0c33af565e1b38fab0c9e3d6a9abb729a
muhammed sule dabban <muhammedsulai4@gmail.com>
2025-10-29 11:25:24 +0100

    arrays

---
+++ src/main/java/Arrays/MultipleArray.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class MultipleArray {
+    public static void main(String[] args) {
+        int[][] array1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] array2 = {{1, 2}, {3, 4, 5}, {6, 7, 8}};
+
+        System.out.println("value in array1 by row are");
+        outputArray(array1);
+
+        System.out.printf("%nvalue in array2 by row are%n");
+        outputArray(array2);
+    }
+
+    public  static void outputArray(int [][] array){
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                System.out.printf("%d ", array[row][column]);
+            }
+            System.out.println();
+        }
+    }
+}
