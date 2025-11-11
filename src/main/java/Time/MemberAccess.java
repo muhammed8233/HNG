3cae381cc1113c50cbaaa3f02ab55265adad616b
muhammed sule dabban <muhammedsulai4@gmail.com>
2025-11-11 11:36:33 +0100

    update time

--- src/main/java/Time/MemberAccess.java
+++ src/main/java/Time/MemberAccess.java
@@ -2,9 +2,9 @@ package Time;
 
 public class MemberAccess {
     public static void main(String[] args) {
-        Time time = new Time();
-        time.hour = 7;
-        time.minute = 15;
-        time.second = 23;
+//        Time time = new Time();
+//        time.hour = 7;
+//        time.minute = 15;
+//        time.second = 23;
     }
 }
--- src/main/java/Time/SimpleTest.java
+++ src/main/java/Time/SimpleTest.java
@@ -1,4 +1,30 @@
 package Time;
 
+import java.util.SimpleTimeZone;
+
 public class SimpleTest {
+    public static void main(String[] args) {
+        SimpleTime simpleTime = new SimpleTime(15,23,34);
+        System.out.println(simpleTime.BuildString());
+    }
+}
+class SimpleTime{
+    private int hour;
+    private int minute;
+    private  int second;
+
+    public SimpleTime(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+
+    }
+    public String BuildString(){
+        return String.format("%24s: %s%n%24s: %s","this.toUniversalString()",
+                this.toUniversalString(), "toUniversalString()", toUniversalString());
+    }
+
+    private String toUniversalString() {
+        return String.format("%02d:%02d:%02d", this.hour, this.minute, this.second);
+    }
 }
